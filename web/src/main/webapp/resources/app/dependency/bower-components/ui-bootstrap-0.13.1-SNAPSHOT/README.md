d184ad86ea26d0303e2b4f68d333cf6c5c1c2b28
volodymyrVY <vhyakymiv@gmail.com>
2015-12-01 17:05:16 +0200

    new updates 4

---
+++ web/src/main/webapp/resources/app/dependency/bower-components/ui-bootstrap-0.13.1-SNAPSHOT/README.md
@@ -0,0 +1,1 @@
+# ui-bootstrap-tpls-0.13.1-SNAPSHOT
--- web/src/main/webapp/resources/app/run.js
+++ web/src/main/webapp/resources/app/run.js
@@ -1,17 +1,57 @@
 /*
-angular.module('runModule',['ui.bootstrap']).config(function($routeProvider){
-    $routeProvider
-        .when('/',{
-            templateUrl:'app/index.html'
+ angular.module('runModule',['ui.bootstrap']).config(function($routeProvider){
+ $routeProvider
+ .when('/',{
+ templateUrl:'app/index.html'
+ })
+ /!*.when('/home',{
+ templateUrl:'app/index.html',
+ controller: 'WelcomeController'
+ })*!/
+ .when('/about', {
+ templateUrl: 'app/welcome/about.html'
+ /!*controller: 'WelcomeController'*!/
+ })
+ .otherwise({ redirectTo: '/'});
+ });*/
+/*angular.module('runModule',['ui.bootstrap']);*/
+
+
+var app = angular.module('runModule', ['ui.bootstrap', 'ui.directives']).config(function () {
+   /* $routeProvider
+        .when('/', {
+            templateUrl: 'app/index.html'
         })
-        /!*.when('/home',{
-            templateUrl:'app/index.html',
-            controller: 'WelcomeController'
-        })*!/
         .when('/about', {
             templateUrl: 'app/welcome/about.html'
-            /!*controller: 'WelcomeController'*!/
-        })
-        .otherwise({ redirectTo: '/'});
-});*/
-angular.module('runModule',['ui.bootstrap']);
+                .otherwise({redirectTo: '/'})
+        })*/
+});
+
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.name = 'World';
+});
+
+app.controller('DropdownCtrl', function ($scope, $log) {
+
+        $scope.items = [
+            'The first choice!',
+            'And another choice for you.',
+            'but wait! A third!'
+        ];
+
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.toggled = function(open) {
+            $log.log('Dropdown is now: ', open);
+        };
+
+        $scope.toggleDropdown = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+    });
